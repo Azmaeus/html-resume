9f8b00f4e702d297704a31067109724edb56c4ee
Azmaeus <falconv@gmail.com>
2022-11-19 00:33:34 -0700

    Update download/print PDF

--- render-pdf.sh
+++ render-pdf.sh
@@ -2,7 +2,7 @@
 
 npx http-server &
 sleep 6;
-chromium --headless --disable-gpu  --run-all-compositor-stages-before-draw --print-to-pdf="./Eric-F-Olsen-Resume-2019.pdf" http://localhost:8080/ --virtual-time-budget=10000;
+chromium --headless --disable-gpu  --run-all-compositor-stages-before-draw --print-to-pdf="./Eric-F-Olsen-Resume-2022.pdf" http://localhost:8080/ --virtual-time-budget=10000;
 kill %1;
 exit;
 
